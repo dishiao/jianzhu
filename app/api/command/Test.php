b898b93a9414da271b3149630481e2cd6651b973
adonis <adonis.di@toursforfun.cn>
2019-07-25 11:20:04 +0800

    init console

---
+++ app/api/command/Test.php
@@ -0,0 +1,23 @@
+<?php
+namespace app\api\command;
+
+use think\console\Command;
+use think\console\Input;
+use think\console\Output;
+use think\Db;
+
+class Test extends Command
+{
+    protected function configure()
+    {
+        $this->setName('test')->setDescription('Here is the remark ');
+    }
+
+    protected function execute(Input $input, Output $output)
+    {
+        $sql = "select * from jz_project limit 1";
+        $res = Db::query($sql);
+        dd($res);
+        $output->writeln("TestCommand:");
+    }
+}
